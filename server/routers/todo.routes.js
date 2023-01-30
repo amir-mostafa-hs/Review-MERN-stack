2b2587e149416980da83b25d3e78f33be878c65d
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-01-30 19:13:09 +0330

    add test route function and test request routes file

--- server/routers/todo.routes.js
+++ server/routers/todo.routes.js
@@ -1,13 +1,26 @@
 // call express router for create app routes
 const router = require("express").Router();
 
+// test route function
+const testFunc = (req, res) => {
+  // console.log(req.body);
+  const resultObj = {};
+  if (req.method === "POST") {
+    resultObj.message = `the route ${req.path} on method ${req.method} is worked`;
+    resultObj.body = req.body;
+  } else {
+    resultObj.message = `the route ${req.path} on method ${req.method} is worked`;
+  }
+  res.json(resultObj);
+};
+
 /**
  * method: GET
  * status: PUBLIC
  * route: /api/v2/
  * description: get all user todo
  */
-router.get("/");
+router.get("/", testFunc);
 
 /**
  * method: GET
@@ -15,7 +28,7 @@ router.get("/");
  * route: /api/v2/:id
  * description: get one user todo
  */
-router.get("/:id");
+router.get("/:id", testFunc);
 
 /**
  * method: POST
@@ -23,7 +36,7 @@ router.get("/:id");
  * route: /api/v2/
  * description: create one user todo
  */
-router.post("/");
+router.post("/", testFunc);
 
 /**
  * method: DELETE
@@ -31,7 +44,7 @@ router.post("/");
  * route: /api/v2/:id
  * description: delete one user todo
  */
-router.delete("/:id");
+router.delete("/:id", testFunc);
 
 /**
  * method: PATCH
@@ -39,7 +52,7 @@ router.delete("/:id");
  * route: /api/v2/:id
  * description: update one user todo
  */
-router.patch("/:id");
+router.patch("/:id", testFunc);
 
 // export all todo routes
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const express = require("express");
 // create express app
 const app = express();
 
+// use express json middleware
+app.use(express.json());
+
 // get .env file property
 const { PORT } = process.env;
 
@@ -15,7 +18,7 @@ const todoRoutes = require("./routers/todo.routes");
 app.get("/", (req, res) => {
   res.json({ message: "welcome to the server app" });
 });
-app.use("/api/v2", todoRoutes);
+app.use("/api/v1", todoRoutes);
 
 // app listener request
 app.listen(PORT, () => console.log(`The server run on port: ${PORT}.\nYou can see the result on http://localhost:${PORT}/`));
